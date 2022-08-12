8f94e1e8f78e9d523414c6a14c19e905015d36e4
Bruno Marques <blmarques.dev@gmail.com>
2022-08-12 17:07:03 -0300

    feat(methods): implement methods to spot manager

--- src/main/java/com/api/parkingmanager/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingmanager/services/ParkingSpotService.java
@@ -1,7 +1,15 @@
 package com.api.parkingmanager.services;
 
+import com.api.parkingmanager.models.ParkingSpotModel;
 import com.api.parkingmanager.repository.ParkingSpotRepository;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class ParkingSpotService {
@@ -11,5 +19,33 @@ public class ParkingSpotService {
     this.parkingSpotRepository = parkingSpotRepository;
   }
 
+  @Transactional
+  public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+    return parkingSpotRepository.save(parkingSpotModel);
+  }
 
+  public boolean existsByLicensePlateCar(String licensePlateCar) {
+    return parkingSpotRepository.existsByLicensePlateCar(licensePlateCar);
+  }
+
+  public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+    return parkingSpotRepository.existsByParkingSpotNumber(parkingSpotNumber);
+  }
+
+  public boolean existsByApartmentAndBlock(String apartment, String block) {
+    return parkingSpotRepository.existsByApartmentAndBlock(apartment, block);
+  }
+
+  public Page<ParkingSpotModel> findAll(Pageable pageable) {
+    return parkingSpotRepository.findAll(pageable);
+  }
+
+  public Optional<ParkingSpotModel> findById(UUID id) {
+    return parkingSpotRepository.findById(id);
+  }
+
+  @Transactional
+  public void delete(ParkingSpotModel parkingSpotModel) {
+    parkingSpotRepository.delete(parkingSpotModel);
+  }
 }
