43dccbabaff690d767aaf980be6dad1a54057916
Bruno Marques <blmarques.dev@gmail.com>
2022-08-12 17:06:02 -0300

    refactor(repository): change from class to interface and implement methods

--- src/main/java/com/api/parkingmanager/repository/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingmanager/repository/ParkingSpotRepository.java
@@ -3,7 +3,12 @@ package com.api.parkingmanager.repository;
 import com.api.parkingmanager.models.ParkingSpotModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
+import org.springframework.stereotype.Repository;
 
-public class ParkingSpotRepository implements JpaRepository<ParkingSpotModel, UUID> {
+@Repository
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotModel, UUID> {
 
+  boolean existsByLicensePlateCar(String licensePlateCar);
+  boolean existsByParkingSpotNumber(String parkingSpotNumber);
+  boolean existsByApartmentAndBlock(String apartment, String block);
 }
