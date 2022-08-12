2b13f2aa1d0f759625069476b703d5b25a57c09c
Bruno Marques <blmarques.dev@gmail.com>
2022-08-12 16:39:58 -0300

    feat(controller): create parking spot controller to router "parking-spot"

--- src/main/java/com/api/parkingmanager/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingmanager/controllers/ParkingSpotController.java
@@ -1,2 +1,19 @@
-package com.api.parkingmanager.controllers;public class ParkingSpotController {
+package com.api.parkingmanager.controllers;
+
+import com.api.parkingmanager.repository.ParkingSpotRepository;
+import com.api.parkingmanager.services.ParkingSpotService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/parking-spot")
+public class ParkingSpotController {
+
+  final ParkingSpotService parkingSpotService;
+
+  public ParkingSpotController(ParkingSpotService parkingSpotService) {
+    this.parkingSpotService = parkingSpotService;
+  }
 }
