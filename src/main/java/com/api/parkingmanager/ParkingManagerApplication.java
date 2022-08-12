20e4550b0cec6a04f9dcfcacdb15058f6cde2c6d
Bruno Marques <blmarques.dev@gmail.com>
2022-08-12 03:43:47 -0300

    feat(setup): setup project and define db connection

---
+++ src/main/java/com/api/parkingmanager/ParkingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.api.parkingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/parkingmanager/ParkingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.parkingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
