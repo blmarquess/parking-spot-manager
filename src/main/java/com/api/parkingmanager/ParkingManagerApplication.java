ec1fbd28df61184dbfa8d9720501fe4b5ef0814c
Bruno Marques <blmarques.dev@gmail.com>
2022-08-12 15:40:41 -0300

    chore(database): config jpa with postgres

--- src/main/java/com/api/parkingmanager/ParkingManagerApplication.java
+++ src/main/java/com/api/parkingmanager/ParkingManagerApplication.java
@@ -2,12 +2,19 @@ package com.api.parkingmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ParkingManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ParkingManagerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String index() {
+		return "hello peoples";
+	}
 }
