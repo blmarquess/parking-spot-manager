20e5ecf555adb69cd748ab7f62afe142edd50d44
Bruno Marques <blmarques.dev@gmail.com>
2022-08-12 16:31:20 -0300

    feat(service): created Service to parking manager

---
+++ src/main/java/Service/ParkingSpotService.java
@@ -0,0 +1,15 @@
+package Service;
+
+import com.api.parkingmanager.repository.ParkingSpotRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+  final ParkingSpotRepository parkingSpotRepository;
+
+  public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+    this.parkingSpotRepository = parkingSpotRepository;
+  }
+
+
+}
